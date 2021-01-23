737e6c1622015b68ba70108f40d8b50b44ded19b
Jesse Federkiewicz <59615077+JesseFederkiewicz@users.noreply.github.com>
2021-01-22 17:34:03 -0700

    Hello, World! - This begins our journey with indepedant control of motors via PWM!

---
+++ Motor Test Code/Basic_Two_Motor_Controller_HBridge_Via_PWM/Basic_Two_Motor_Controller_HBridge.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		Basic_Two_Motor_Controller_HBridge.ino
+ Created:	1/22/2021 4:44:22 PM
+ Author:	Jesse Federkiewicz
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "Functions.h"
+void setup() {
+	setupFuncTestCpp();
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	motorLogicCpp();
+}
