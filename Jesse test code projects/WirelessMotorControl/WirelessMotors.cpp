c73b86b59b9e9b546e5093cc2bbf645dec856aa6
Jesse Federkiewicz <59615077+JesseFederkiewicz@users.noreply.github.com>
2021-02-04 18:51:40 -0700

    independant wheel control with simple web logic

--- Jesse test code projects/WirelessMotorControl/WirelessMotors.cpp
+++ Jesse test code projects/WirelessMotorControl/WirelessMotors.cpp
@@ -56,33 +56,115 @@ void PWMSetup(Motor_Settings m, mcpwm_config_t* unitConf)
 	mcpwm_capture_enable(m.pwm.unit, m.encoder.capSignal, m.encoder.edgeCapture, m.encoder.preScale);
 }
 
+class MotorDuty
+{
+	public:
+		int frontLeftMotorDuty;
+		int frontRightMotorDuty;
+		int backLeftMotorDuty;
+		int backRightMotorDuty;
+};
+
+MotorDuty SimpleSteering(int dbX, int dbY)
+{
+	MotorDuty motorSetting;
+
+	if (dbX > 0)
+	{
+		motorSetting.frontLeftMotorDuty = 100;
+		motorSetting.frontRightMotorDuty = 0;
+		motorSetting.backLeftMotorDuty = 100;
+		motorSetting.backRightMotorDuty = 0;
+	}
+
+	else if (dbX < 0)
+	{
+		motorSetting.frontLeftMotorDuty = 0;
+		motorSetting.frontRightMotorDuty = 100;
+		motorSetting.backLeftMotorDuty = 0;
+		motorSetting.backRightMotorDuty = 100;
+	}
+
+	else
+	{
+		motorSetting.frontLeftMotorDuty = 0;
+		motorSetting.frontRightMotorDuty = 0;
+		motorSetting.backLeftMotorDuty = 0;
+		motorSetting.backRightMotorDuty = 0;
+	}
+
+	if (dbY == 100)
+	{
+		motorSetting.frontLeftMotorDuty = 100;
+		motorSetting.frontRightMotorDuty = 100;
+		motorSetting.backLeftMotorDuty = 100;
+		motorSetting.backRightMotorDuty = 100;
+	}
+	//else
+	//{
+	//	motorSetting.frontLeftMotorDuty = 0;
+	//	motorSetting.frontRightMotorDuty = 0;
+	//	motorSetting.backLeftMotorDuty = 0;
+	//	motorSetting.backRightMotorDuty = 0;
+	//}
+
+
+	return motorSetting;
+}
+
 void Main()
 {
 	// motor one configs
-	Motor_Settings m1;
-	m1.pwm.unit = MCPWM_UNIT_0;               // m1 using pwm unit 0
-	m1.pwm.timer = MCPWM_TIMER_0;             // m1 using unit 0 timer 0
-	m1.pwm.opOut = MCPWM_OPR_A;               // m1 on operator 0 output A
-	m1.pwm.signal = MCPWM0A;                  // sort of the same as above, needed for gpio_init
-	m1.pwm.pin = 22;                          // pwm for m1 on pin 22
-	m1.encoder.pin = 34;                      // encoder input on pin 34
-	m1.encoder.signal = MCPWM_CAP_0;          // encoder io signal
-	m1.encoder.capSignal = MCPWM_SELECT_CAP0; // same as above but for mcpwm_capture_enable()
-	m1.encoder.edgeCapture = MCPWM_POS_EDGE;  // capture positive edges
-	m1.encoder.preScale = 1;                  // no scaling of capture clock for fast reading response
+	Motor_Settings frontLeftMotor;
+	frontLeftMotor.pwm.unit = MCPWM_UNIT_0;               // frontLeftMotor using pwm unit 0
+	frontLeftMotor.pwm.timer = MCPWM_TIMER_0;             // frontLeftMotor using unit 0 timer 0
+	frontLeftMotor.pwm.opOut = MCPWM_OPR_A;               // frontLeftMotor on operator 0 output A
+	frontLeftMotor.pwm.signal = MCPWM0A;                  // sort of the same as above, needed for gpio_init
+	frontLeftMotor.pwm.pin = 22;                          // pwm for frontLeftMotor on pin 22
+	frontLeftMotor.encoder.pin = 34;                      // encoder input on pin 34
+	frontLeftMotor.encoder.signal = MCPWM_CAP_0;          // encoder io signal
+	frontLeftMotor.encoder.capSignal = MCPWM_SELECT_CAP0; // same as above but for mcpwm_capture_enable()
+	frontLeftMotor.encoder.edgeCapture = MCPWM_POS_EDGE;  // capture positive edges
+	frontLeftMotor.encoder.preScale = 1;                  // no scaling of capture clock for fast reading response
 
 	// motor two configs
-	Motor_Settings m2;
-	m2.pwm.unit = m1.pwm.unit;                // m2 shares unit with m1
-	m2.pwm.timer = m1.pwm.timer;              // m2 shares timer with m1
-	m2.pwm.opOut = MCPWM_OPR_B;               // m2 on operator 0 output B
-	m2.pwm.signal = MCPWM0B;                  // needed for gpio_init
-	m2.pwm.pin = 23;		                  // pwm for m1 on pin 23
-	m2.encoder.pin = 35;                      // encoder input on pin 35
-	m2.encoder.signal = MCPWM_CAP_1;		  // encoder io signal
-	m2.encoder.capSignal = MCPWM_SELECT_CAP1; // same as above but for mcpwm_capture_enable()
-	m2.encoder.edgeCapture = MCPWM_POS_EDGE;  // capture positive edges
-	m2.encoder.preScale = 1;                  // no scaling of capture clock for fast reading response
+	Motor_Settings frontRightMotor;
+	frontRightMotor.pwm.unit = frontLeftMotor.pwm.unit;                // frontRightMotor shares unit with frontLeftMotor
+	frontRightMotor.pwm.timer = frontLeftMotor.pwm.timer;              // frontRightMotor shares timer with frontLeftMotor
+	frontRightMotor.pwm.opOut = MCPWM_OPR_B;               // frontRightMotor on operator 0 output B
+	frontRightMotor.pwm.signal = MCPWM0B;                  // needed for gpio_init
+	frontRightMotor.pwm.pin = 23;		                  // pwm for frontLeftMotor on pin 23
+	frontRightMotor.encoder.pin = 35;                      // encoder input on pin 35
+	frontRightMotor.encoder.signal = MCPWM_CAP_1;		  // encoder io signal
+	frontRightMotor.encoder.capSignal = MCPWM_SELECT_CAP1; // same as above but for mcpwm_capture_enable()
+	frontRightMotor.encoder.edgeCapture = MCPWM_POS_EDGE;  // capture positive edges
+	frontRightMotor.encoder.preScale = 1;                  // no scaling of capture clock for fast reading response
+
+	// motor three configs
+	Motor_Settings backLeftMotor;
+	backLeftMotor.pwm.unit = MCPWM_UNIT_1;                // frontRightMotor shares unit with frontLeftMotor
+	backLeftMotor.pwm.timer = MCPWM_TIMER_0;              // frontRightMotor shares timer with frontLeftMotor
+	backLeftMotor.pwm.opOut = MCPWM_OPR_A;               // frontRightMotor on operator 0 output B
+	backLeftMotor.pwm.signal = MCPWM0A;                  // needed for gpio_init
+	backLeftMotor.pwm.pin = 15;		                  // pwm for frontLeftMotor on pin 23
+	backLeftMotor.encoder.pin = 12;                      // encoder input on pin 35
+	backLeftMotor.encoder.signal = MCPWM_CAP_1;		  // encoder io signal
+	backLeftMotor.encoder.capSignal = MCPWM_SELECT_CAP1; // same as above but for mcpwm_capture_enable()
+	backLeftMotor.encoder.edgeCapture = MCPWM_POS_EDGE;  // capture positive edges
+	backLeftMotor.encoder.preScale = 1;                  // no scaling of capture clock for fast reading response
+
+	// motor four configs
+	Motor_Settings backRightMotor;
+	backRightMotor.pwm.unit = backLeftMotor.pwm.unit;                // frontRightMotor shares unit with frontLeftMotor
+	backRightMotor.pwm.timer = backLeftMotor.pwm.timer;              // frontRightMotor shares timer with frontLeftMotor
+	backRightMotor.pwm.opOut = MCPWM_OPR_B;               // frontRightMotor on operator 0 output B
+	backRightMotor.pwm.signal = MCPWM0B;                  // needed for gpio_init
+	backRightMotor.pwm.pin = 2;			                  // pwm for frontLeftMotor on pin 23
+	backRightMotor.encoder.pin = 14;                      // encoder input on pin 35
+	backRightMotor.encoder.signal = MCPWM_CAP_0;		  // encoder io signal
+	backRightMotor.encoder.capSignal = MCPWM_SELECT_CAP0; // same as above but for mcpwm_capture_enable()
+	backRightMotor.encoder.edgeCapture = MCPWM_POS_EDGE;  // capture positive edges
+	backRightMotor.encoder.preScale = 1;                  // no scaling of capture clock for fast reading response
 
 	// config for pwm unit 0 timer 0 
 	mcpwm_config_t pwmconf;
@@ -93,27 +175,43 @@ void Main()
 	pwmconf.cmpr_b = 0;
 
 	// run setup for each motor
-	PWMSetup(m1, &pwmconf);
-	PWMSetup(m2, &pwmconf);
+	PWMSetup(frontLeftMotor, &pwmconf);
+	PWMSetup(frontRightMotor, &pwmconf);
+	PWMSetup(backLeftMotor, &pwmconf);
+	PWMSetup(backRightMotor, &pwmconf);
 
 	// testing encoder readings
 	uint encRead1 = 0;
 	uint encRead2 = 0;
+	uint encRead3 = 0;
+	uint encRead4 = 0;
 
-	// setup gpio pin 1 for direction control
-	gpio_num_t dirPin = GPIO_NUM_1;
-	gpio_config_t dirConfig;
-	dirConfig.intr_type = GPIO_INTR_DISABLE;
-	dirConfig.mode = GPIO_MODE_OUTPUT;
-	dirConfig.pin_bit_mask = 0b10; // bit #1 for pin 1
-	dirConfig.pull_down_en = GPIO_PULLDOWN_ENABLE;
-	dirConfig.pull_up_en = GPIO_PULLUP_ENABLE;
+	// setup gpio pin 1 for rightDirection control
+	gpio_num_t dirPinRight = GPIO_NUM_1;
+	gpio_config_t dirConfigRight;
+	dirConfigRight.intr_type = GPIO_INTR_DISABLE;
+	dirConfigRight.mode = GPIO_MODE_OUTPUT;
+	dirConfigRight.pin_bit_mask = 0b10; // bit #1 for pin 1
+	dirConfigRight.pull_down_en = GPIO_PULLDOWN_ENABLE;
+	dirConfigRight.pull_up_en = GPIO_PULLUP_ENABLE;
 
-	gpio_config(&dirConfig);
+	// setup gpio pin 32 for leftDirectionDirection control
+	gpio_num_t dirPinLeft = GPIO_NUM_0;
+	gpio_config_t dirConfigLeft;
+	dirConfigLeft.intr_type = GPIO_INTR_DISABLE;
+	dirConfigLeft.mode = GPIO_MODE_OUTPUT;
+	dirConfigLeft.pin_bit_mask = 0b01; // bit #1 for pin 1
+	dirConfigLeft.pull_down_en = GPIO_PULLDOWN_ENABLE;
+	dirConfigLeft.pull_up_en = GPIO_PULLUP_ENABLE;
+
+	gpio_config(&dirConfigRight);
+	gpio_config(&dirConfigLeft);
 
 	// set gpio 1 high for now
-	int direction = 1;
-	gpio_set_level(GPIO_NUM_1, direction);
+	int rightDirection = 1;
+	int leftDirection = rightDirection;
+	gpio_set_level(GPIO_NUM_1, rightDirection);
+	gpio_set_level(GPIO_NUM_32, leftDirection);
 
 	char* ssid = "Cappy";
 	const char* password = "ThisIs@nAdequateP@ss123";
@@ -121,13 +219,13 @@ void Main()
 	const char* postArgs = "action=GrabXYTimeStamp&carID=1";
 	const char* server = "thor.net.nait.ca";
 
-	//Serial.begin(115200);
+	Serial.begin(115200);
 	WiFi.begin(ssid, password);
 
 	//Serial.print("Connecting");
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(500);
-		Serial.print(".");
+		//Serial.print(".");
 	}
 	//Serial.println();
 
@@ -149,38 +247,39 @@ void Main()
 				http.begin(webService);
 
 			http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+			delay(2);
 			int httpResponseCode = http.POST(postArgs);
+			delay(200);
 
 			if (httpResponseCode > 0)
 			{
-				//Serial.println("HTTP Response code: ");
-				//Serial.println(httpResponseCode);
+				Serial.println(httpResponseCode);
+				String payload = http.getString();		//causing crash and reboot ocasionally (try catch no fix)
+				delay(50);
 
-				String payload = http.getString();
-				delay(20);
-				//Serial.println("Payload: ");
 				jason = JSON.parse(payload);
-				//Serial.println(jason["xCoord"]);
-				//Serial.println(jason["yCoord"]);
-				//Serial.println(jason["timeStamp"]);
+
 				if (timeStamp != atoi(jason["timeStamp"]))
 				{
 					xCoord = atoi(jason["xCoord"]);
 					yCoord = atoi(jason["yCoord"]);
 					timeStamp = atoi(jason["timeStamp"]);
 				}
-
-				//timeStamp = jason["timeStamp"];
+								
 			}
 
-		
+			MotorDuty motorData = SimpleSteering(xCoord, yCoord);
 
-	/*		mcpwm_set_duty(m1.pwm.unit, m1.pwm.timer, m1.pwm.opOut, atoi(jason["xCoord"]));
-			mcpwm_set_duty(m2.pwm.unit, m2.pwm.timer, m2.pwm.opOut, atoi(jason["yCoord"]));*/
-			mcpwm_set_duty(m1.pwm.unit, m1.pwm.timer, m1.pwm.opOut, xCoord);
-			mcpwm_set_duty(m2.pwm.unit, m2.pwm.timer, m2.pwm.opOut, yCoord);
+			Serial.println(motorData.frontLeftMotorDuty);
+			Serial.println(motorData.frontRightMotorDuty);
+			Serial.println(motorData.backLeftMotorDuty);
+			Serial.println(motorData.backRightMotorDuty);
+			Serial.println();
 
-			
+			mcpwm_set_duty(frontLeftMotor.pwm.unit, frontLeftMotor.pwm.timer, frontLeftMotor.pwm.opOut, motorData.frontLeftMotorDuty);
+			mcpwm_set_duty(frontRightMotor.pwm.unit, frontRightMotor.pwm.timer, frontRightMotor.pwm.opOut, motorData.frontRightMotorDuty);			
+			mcpwm_set_duty(backLeftMotor.pwm.unit, backLeftMotor.pwm.timer, backLeftMotor.pwm.opOut, motorData.backLeftMotorDuty);
+			mcpwm_set_duty(backRightMotor.pwm.unit, backRightMotor.pwm.timer, backRightMotor.pwm.opOut, motorData.backRightMotorDuty);
 		}		
 	}
 }
